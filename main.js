b3f6ea79567b90233142f0ea044439517e0dfc10
LETÍCIA ETIELE SILVA <78575275+LeticiaEtiele@users.noreply.github.com>
2022-09-13 13:51:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	
+
+	instializeInSetup(mario);
+}
+   
+function draw() {
+	game();
+}
